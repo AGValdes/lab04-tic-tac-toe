74d16d18822f68bd90afa2745d05f9954cc24249
Ameilia Valdes <valdes.ameilia@gmail.com>
2021-01-14 20:37:53 -0800

    couldnt get checkforwinner to work but got checkswitch to work. had to change all of the classes to public for the tests to run, thats good to know

--- Lab 04 tests/UnitTest1.cs
+++ Lab 04 tests/UnitTest1.cs
@@ -1,14 +1,49 @@
 using System;
 using Xunit;
+using Lab04_TicTacToe.Classes;
+using Lab04_TicTacToe;
 
 namespace Lab_04_tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void TestForAWinner()
         {
+           /* Player player1 = new Player();
+            player1.Name = "jane";
+            player1.Marker = "O";
 
+            Player player2 = new Player();
+            player2.Name = "doe";
+            player2.Marker = "X";
+
+            Game newGame = new Game(player1, player2);
+
+            Board testBoard = new Board();
+            testBoard.GameBoard = 
+             
+
+
+            Assert.True(newGame.CheckForWinner());*/
+        }
+
+        [Fact]
+        public void CheckSwitch()
+        {
+            Player player1 = new Player();
+            player1.Name = "Jane";
+            player1.Marker = "X";
+            player1.IsTurn = true;
+
+            Player player2 = new Player();
+            player2.Name = "Doe";
+            player2.IsTurn = false;
+
+            Game newgame = new Game(player1, player2);
+            newgame.SwitchPlayer();
+
+            Assert.True(player2.IsTurn);
         }
     }
 }
